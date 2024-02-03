498fe9e91f9c1938ed0a4cb33fa9bc88aee1f0ee
karikumutimahonore <karikumutimahonore778@gmail.com>
2024-02-02 23:41:54 -0800

    addition of integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,47 +1,17 @@
 #!/usr/bin/python3
-
-"""
-Module with a function that adds two numbers. It checks for the validity of
-arguments given as operands before trying to add them. In the event of an
-error, an exception is raised. Notable amongst these exceptions is the
-TypeError which is raised when the operands fail to be an integer.
-"""
+"""Defines an integer addition function."""
 
 
-def add_integer(a: float | int, b: float | int = 98) -> int:
-    """Returns the sum of two integers.
+def add_integer(a, b=98):
+    """Return the integer addition of a and b.
 
-    Args:
-        a (float | int): The first number.
-        b (float | int, optional): The second number. Defaults to 98.
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: When the operands given are not of a valid type (integers).
-
-    Returns:
-        int: The sum of the two numbers as an integer.
-
-    Tests:
-        >>> add_integer(0)
-        98
-        >>> add_integer(1, -2)
-        -1
-        >>> add_integer('1', '67')
-        Traceback (most recent call last):
-        ...
-        TypeError: a must be an integer
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, (int, float)):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-
-    if not isinstance(b, (int, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    # ensure the operands are of integer type
-    try:
-        int(a)
-        int(b)
-    except Exception as err:
-        raise err
-
-    return int(a) + int(b)
+    return (int(a) + int(b))
